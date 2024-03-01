2eac0e513b6a9829589d78146c80d352c24a7dee
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-01 13:07:17 +0200

    first commit

--- apps/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def index(request):
-    return render(request, 'main/index.html')
-
-
-def lol(request):
-    return render(request, 'main/lol.html')
