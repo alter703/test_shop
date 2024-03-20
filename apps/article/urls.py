97476f17f45876c3edb61b4465ea04cd2334583d
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-20 17:18:10 +0200

    Add and set up article app, update base template, settings and urls in core

---
+++ apps/article/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'article'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+
+]
--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
+    path('article/', include('apps.article.urls')),
 ]
