87383c98b1b63e09803b8073c19a00101ea314a6
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-20 17:51:01 +0200

    Update color of 'Create Post' button, add POst model and migrate it, add Postform form in article app, update look of article templates

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -6,5 +6,6 @@ app_name = 'article'
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('post/<int:post_id>', views.detail, name='detail'),
+    path('create/', views.create_view, name='create'),
 ]
--- apps/article/views.py
+++ apps/article/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'article/index.html')
+
+    all_posts = Post.objects.all()
+    amount_posts = Post.objects.count()
+
+    context = {
+        'all_posts': all_posts,
+        'created_form': PostForm(),
+        'amount_posts': amount_posts,
+    }
+
+    return render(request, 'article/index.html', context)
+
+def detail(request, post_id):
+    
+    post = get_object_or_404(Post, pk=post_id)
+    
+    context = {
+        'post': post
+    }
+    
+    return render(request, 'article/detail.html', context)
+
+def create_view(request):
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('article:detail' , post_id=post.id)
