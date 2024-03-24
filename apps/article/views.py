6a8dbae3354b25b83791d8f0d17e3ac78c741ac8
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-24 12:01:24 +0200

    Add Paginator to index template and views in article app

--- apps/article/views.py
+++ apps/article/views.py
@@ -3,14 +3,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 
+from django.core.paginator import Paginator
+
 # Create your views here.
 def index(request):
 
     all_posts = Post.objects.all()
     amount_posts = Post.objects.count()
 
+    paginator = Paginator(all_posts, 3)
+    page = request.GET.get('page')
+    all_posts_page = paginator.get_page(page)
+
     context = {
-        'all_posts': all_posts,
+        'all_posts': all_posts_page,
         'created_form': PostForm(),
         'amount_posts': amount_posts,
     }
