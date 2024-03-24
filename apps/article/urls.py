114f0c651b39231b4a30495a7e423c2689d5f076
Anatoliy <anatoliy.brazh130@gmail.com>
2024-03-24 12:15:37 +0200

    Update article views and urls, update ability to edit and delete post if user is author of this

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:post_id>', views.detail, name='detail'),
     path('create/', views.create_view, name='create'),
+    
+    path('delete/<int:post_id>/', views.delete_view, name='delete'),    #google.com/blog/delete/1/
+    path('update/<int:post_id>/', views.update_view, name='update'),    #google.com/blog/update/1/
 ]
--- apps/article/views.py
+++ apps/article/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 from .forms import PostForm
+
+from django.contrib import messages
 
 from django.core.paginator import Paginator
 
@@ -23,14 +26,16 @@ def index(request):
 
     return render(request, 'article/index.html', context)
 
+
 def detail(request, post_id):
-    
     post = get_object_or_404(Post, pk=post_id)
     
+    post = Post.objects.get(pk=post_id)
+    update_form = PostForm(instance=post)
     context = {
-        'post': post
+        'post': post,
+        'update_form': update_form
     }
-    
     return render(request, 'article/detail.html', context)
 
 def create_view(request):
@@ -42,3 +47,24 @@ def create_view(request):
             post.author = request.user
             post.save()
             return redirect('article:detail' , post_id=post.id)
+        
+@login_required
+def delete_view(request, post_id):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, pk=post_id, author=request.user)
+        post.delete()
+        messages.success(request, 'Post deleted successfully')
+    return redirect('article:index')
+
+@login_required
+def update_view(request, post_id):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, pk=post_id, author=request.user)
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Post updated successfully')
+        else:
+            messages.error(request, 'Error updating post')
+        return redirect('article:detail', post_id=post.id)
+    return redirect('article:index')
