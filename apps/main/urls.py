9dc11e0e0ecde97c2b71035af5095a737b483d32
Anatoliy <anatoliy.brazh130@gmail.com>
2024-04-08 22:49:00 +0300

    Add test class-based view IndexView in main app

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('about/', views.about, name='about'),
     path('lol/', views.lol, name='lol'),
 
